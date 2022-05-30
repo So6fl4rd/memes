b004a4d73961957b8816cf3513069efde42e275e
So6fl4rd <victorbelli@hotmail.fr>
2022-05-30 02:06:06 +0200

    the spicy window

--- SpicyGame/OGL3D/source/OGL3D/Window/OpenGLWindow.cpp
+++ SpicyGame/OGL3D/source/OGL3D/Window/OpenGLWindow.cpp
@@ -1,28 +1,56 @@
 #include <OGL3D/Window/OpenGLWindow.h>
+#include <OGL3D/Game/OpenGLGame.h>
 #include <Windows.h>
 #include <assert.h>
+#include <iostream>
 
-OpenGLWindow::OpenGLWindow() {
-    WNDCLASSEX wc = {};
-    wc.cbSize = sizeof(WNDCLASSEX);
-    wc.lpszClassName = (LPCTSTR) "OpenGLWindow";
-    wc.lpfnWndProc = DefWindowProc;
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_DESTROY:
+	{
+		OpenGLWindow* window = (OpenGLWindow*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+        std::cout << "DESTROY MSG" << std::endl;
+		break;
+	}
+	case WM_CLOSE:
+	{
+		PostQuitMessage(0);
+        std::cout << "CLOSE MSG" << std::endl;
+		break;
+	}
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
 
-    assert(RegisterClassEx(&wc));
 
-    RECT rc = {0, 0, 1024, 768};
-    AdjustWindowRect(&rc, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, false);
+OpenGLWindow::OpenGLWindow()
+{
+	WNDCLASSEX wc = {};
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.lpszClassName = L"OGL3DWindow";
+	wc.lpfnWndProc = &WndProc;
 
-    m_handle = CreateWindowEx(0, L"OpenGLWindow", L"Da spicy game window",WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
-        CW_USEDEFAULT, CW_USEDEFAULT, rc.right-rc.left,rc.bottom-rc.top, NULL, NULL, NULL, NULL);
+	auto classId = RegisterClassEx(&wc);
+	assert(classId);
 
-    assert(m_handle);
+	RECT rc = { 0,0,1024,768 };
+	AdjustWindowRect(&rc, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, false);
 
-    ShowWindow((HWND)m_handle, SW_SHOW);
-    UpdateWindow((HWND)m_handle);
+    m_handle = CreateWindowEx(0, MAKEINTATOM(classId), L"Da Spicy Game Window", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU, CW_USEDEFAULT, CW_USEDEFAULT,
+		rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, NULL, NULL);
 
+	assert(m_handle);
+
+	SetWindowLongPtr((HWND)m_handle, GWLP_USERDATA, (LONG_PTR)this);
+
+	ShowWindow((HWND)m_handle, SW_SHOW);
+	UpdateWindow((HWND)m_handle);
 }
 
 OpenGLWindow::~OpenGLWindow() {
-    DestroyWindow((HWND)m_handle);
+	DestroyWindow(HWND(m_handle));
 }
